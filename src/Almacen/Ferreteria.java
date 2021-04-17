0dd7e5f3423c8e4d856da2f8e1d2553f5fd7d5b9
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-04-16 19:27:35 -0500

    Creé mis 2 clases
    cree la clase Ferretería y Proveedor

--- src/Almacen/Ferreteria.java
+++ src/Almacen/Ferreteria.java
@@ -1,5 +1,26 @@
 package Almacen;
 
 public class Ferreteria {
+	private String Direccion;
+	private Dueño dueño;
+	private long NIT;
+	private String nombre;
+	private Empleado empleados[];
+	private Proveedor proveedores[];
+	private Inventario inventario[];
+	private Cliente cliente[];
+	private Facturas Factura[];
+	
+	
+	
+	public void abrirFerreteria(){
+		
+	}
 
+	
+	public void cerrarFerreteria(){
+		
+		
+	}
+	
 }
---
+++ src/Almacen/Proveedores.java
@@ -0,0 +1,19 @@
+package Almacen;
+
+public class Proveedores {
+	private String nombre;
+	private int cedula;	
+	private String nombre_Empresa;
+	private int NIT;
+	
+	public void entregarProductos(){
+		
+		
+		
+	}
+
+	
+	public void recogerDefectuosos(){
+		
+	}
+}
