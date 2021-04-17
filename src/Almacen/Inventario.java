f4ab2f9aea757f8a4cce85d6370d259b15dba56a
Haiber Cordoba <hacordobab@unal.edu.co>
2021-04-16 19:21:34 -0500

    2.1 Creacion de mis dos clases
    Creacion de clase inventario y producto con sus metodos y atributos

--- src/Almacen/Inventario.java
+++ src/Almacen/Inventario.java
@@ -1,5 +1,25 @@
 package Almacen;
 
-public class Inventario {
+import java.util.ArrayList;
 
+public class Inventario {
+	private ArrayList<String> producto;
+    private int cantidad;
+    private Ferreteria almacen;
+    
+    
+    public Inventario(ArrayList<String> producto,int cantidad,Ferreteria almacen) {
+    	this.producto = producto;
+    	this.cantidad = cantidad;
+    	this.almacen = almacen;
+    }
+    public void agregarProducto() {
+    	
+    }
+    public void quitarProducto() {
+    	
+    }
+    public void verificarProducto() {
+    	
+    }
 }
--- src/Almacen/Producto.java
+++ src/Almacen/Producto.java
@@ -1,5 +1,25 @@
 package Almacen;
 
 public class Producto {
+	private int referencia;
+	private String nombre;
+	private double precio;
+	private boolean estado;
+	
+	public Producto(int referencia,String nombre,double precio,boolean estado) {
+		this.referencia = referencia;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.estado = estado;
+	
+		
+	}
+	
+	public void SeDaña() {
+		System.out.println("Producto dañado no apto para la venta");
+		
+		
+	}
+	
 
 }
