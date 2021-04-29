0406ca9784703b13971ba6d3dc908d092ff246c6
Haiber Cordoba <hacordobab@unal.edu.co>
2021-04-28 21:10:18 -0500

    Creacion de la sub clase Materialdeconstruccion
    Creacion de la sub clase Materialdeconstruccion

---
+++ src/Almacen/MaterialDeConstruccion.java
@@ -0,0 +1,11 @@
+package Almacen;
+
+public class MaterialDeConstruccion extends Producto {
+	Herramientas[] MaterialesDeCostruccion;
+	boolean Perecedero;
+	
+	public  MaterialDeConstruccion() {
+		
+	}
+
+}
