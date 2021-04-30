6bcdbe5241667aa1717cd083038301f27204b908
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-04-29 21:45:15 -0500

    Update Main.java

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -9,16 +9,17 @@ public class Main {
 		System.out.println("//BIENVEDIOS AL SOFTWARE DE LA FERRETERÍA//");
 		System.out.println("------------------------------------------");
 		System.out.println("Selecciona el número correspondiente a la funcionalidad");
-		System.out.println("1.Vender producto"); //Inventario productos domiciliario cliente factura
+		System.out.println("1.Registrar cliente"); // cuando se crea un cliente se le va a regalar un material de construccion o una herramienta que se le ofrezca de manera aletoria, suponermos que todos los clientes que se vayan a registrar van a comprar otras cosas
 		System.out.println("2.Devolver producto"); //tomar numero factura ---> revisar si existe ---> meter productos a inventario ---> verificar si se devuelven todos o no ---> en caso de devolver todos los productos se anula la factura
-		System.out.println("3. Mostrar ranking empleados"); //
-		System.out.println("4. funcionalidad");
-		System.out.println("5. funcionalidad");
+		System.out.println("3. Vender producto"); // Se pedile la cedula--> en caso de no estar registrado el programa para e imprime "cliente no registrado precione la opcion 1" --->. En caso de estar registrado,escribir el nombre del empleado que lo atendio, pedir los productos de la siguiente manera "referencia, cantidad",revisar en inventario las cantidades si se puede satisfacer el producto imprime ok si no ingrese se imprimira que cantidades insuficientes y cuantas hay en inventario. Oprimir 0,0 para parar. Luego mostrar el total. 
+		System.out.println("4.");
+		System.out.println("5.");
 		System.out.println("Presione  6 para salir");
 		
 		Scanner input = new Scanner(System.in);
 		
 		int opcion = 0;
+		
 		
 		do{
 			opcion = input.nextInt();
