e1570c654ee121a5da74e9de400c98a21d50ed98
santy028 <79714057+santy028@users.noreply.github.com>
2021-08-02 18:59:12 -0500

    metodo abstracto

--- src/Almacen/MaterialDeConstruccion.java
+++ src/Almacen/MaterialDeConstruccion.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class MaterialDeConstruccion extends Producto {
 	static ArrayList<MaterialDeConstruccion> material= new ArrayList<MaterialDeConstruccion>() ;
 	boolean perecedero;
+	String medida;
 	
-	public  MaterialDeConstruccion(int referencia, String nombre, double precio, boolean estado,boolean perecedero) {
-		super(referencia, nombre, precio, estado);
+	public  MaterialDeConstruccion(int referencia, String nombre, double precio, boolean estado,boolean perecedero,String medida) {
+		super(referencia, nombre, precio, estado); //uso de herencia
 		
 		this.perecedero=perecedero;
 		material.add(this);
@@ -17,5 +18,11 @@ public class MaterialDeConstruccion extends Producto {
 	    Inventario.productos.add(a);
 
 }
+
+	@Override
+	public String FormadeVenta() {
+		
+		return medida;
+	}
 
 }
